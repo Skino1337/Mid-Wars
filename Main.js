7277d3d29b6c8c00d786ea9e2b34256e260c835d
Skino1337 <Skino1337@gmail.com>
2013-09-01 17:43:55 -0700

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,85 +1,75 @@
 game.hook("OnGameFrame", onGameFrame);
 game.hook("OnMapStart", onMapStart);
 
+game.hookEvent("dota_player_kill", onPlayerKill);
+game.hookEvent("dota_player_killed", onPlayerKilled);
+
 //console.addClientCommand("pos", pos);
 
-var init, msgPrinted;
+var init;
 var changeRespawnTime = [];
 var radiCour = [], direCour = [];
 var playerManager;
- 
-function onGameFrame()
-{
-	if (!init)
-	{
+var playersGold = [];
+for (var i = 0; i < dota.MAX_PLAYERS; i++) {playersGold[i] = [];};
+	
+function onGameFrame(){
+	if (!init){
 		console.findConVar("dota_disable_top_lane").setInt(1);
 		console.findConVar("dota_disable_bot_lane").setInt(1);
 		console.findConVar("dota_easy_mode").setInt(1);
 			
 		init = true;
 	}
-	
-	if (!msgPrinted && game.rules.props.m_nGameState == dota.STATE_GAME_IN_PROGRESS)
-	{
-		printToAll("[MW] Mid Wars by Skino.");
-		
-		msgPrinted = true;
-	}
-	
-	var clients = getConnectedPlayingClients();
-	for (var i in clients)
-	{
-		var client = clients[i];
+
+	var clients = getClients();
+	for (var i = 0; i < clients.length; i++){
+		var client = clients[i];		
+		var playerID = client.netprops.m_iPlayerID;
 		
 		var hero = client.netprops.m_hAssignedHero;
 		if (!hero) continue;
-		
-		var playerId = client.netprops.m_iPlayerID;
-		
-		if (hero.netprops.m_flRespawnTime > 1 && !hero.netprops.m_bReincarnating)
-		{
-			if (!changeRespawnTime[playerId])
-			{
+
+		playersGold[playerID]["Radiant"] = playerManager.netprops.m_iUnreliableGoldRadiant[playerID];
+		playersGold[playerID]["Dire"] = playerManager.netprops.m_iUnreliableGoldDire[playerID];
+
+		if (hero.netprops.m_flRespawnTime > 1 && !hero.netprops.m_bReincarnating){
+			if (!changeRespawnTime[playerID]){
 				resptime = hero.netprops.m_flRespawnTime;
 				deadtime = game.rules.props.m_fGameTime;
-				time = (resptime - deadtime) * 0.15;
+				time = (resptime - deadtime) * 0.20;
 				
 				hero.netprops.m_flRespawnTime = deadtime + time;
 				
-				changeRespawnTime[playerId] = true;
+				changeRespawnTime[playerID] = true;
 			}
 		}
-		else
-		{
-			changeRespawnTime[playerId] = false;
+		else{
+			changeRespawnTime[playerID] = false;
 		}
 		
 		var team = client.netprops.m_iTeamNum;
-		if (!radiCour["Give"] && team == dota.TEAM_RADIANT)
-		{
+		if (!radiCour["Give"] && team == dota.TEAM_RADIANT){
 			radiCour["Item"] = dota.giveItemToHero("item_courier", hero);
 			radiCour["Hero"] = hero;
-			radiCour["PlayerID"] = playerId;
+			radiCour["PlayerID"] = playerID;
 			radiCour["Give"] = true;
 			radiCour["Use"] = false;
 			dota.giveItemToHero("item_flying_courier", hero);
 		}
-		if (!direCour["Give"] && team == dota.TEAM_DIRE)
-		{
+		if (!direCour["Give"] && team == dota.TEAM_DIRE){
 			direCour["Item"] = dota.giveItemToHero("item_courier", hero);
 			direCour["Hero"] = hero;
-			direCour["PlayerID"] = playerId;
+			direCour["PlayerID"] = playerID;
 			direCour["Give"] = true;
 			direCour["Use"] = false;
 			dota.giveItemToHero("item_flying_courier", hero);
 		}
 	}
 	
-	if (!radiCour["Use"] || !direCour["Use"])
-	{
+	if (!radiCour["Use"] || !direCour["Use"]){
 		var couriers = game.findEntitiesByClassname("npc_dota_courier");
-		for (var i in couriers)
-		{
+		for (var i = 0; i < couriers.length; i++){
 			var cour = couriers[i];
 			var team = cour.netprops.m_iTeamNum;
 			
@@ -98,57 +88,60 @@ function onGameFrame()
 		dota.executeOrders(direCour["PlayerID"], dota.ORDER_TYPE_CAST_ABILITY_NO_TARGET, [direCour["Hero"]], null, direCour["Item"], false, 0, 0, 0);
 }
 
-function onMapStart() 
-{
+function onMapStart() {
 	playerManager = game.findEntityByClassname(-1, "dota_player_manager");
 	
 	dota.removeAll("npc_dota_barracks");
 	dota.removeAll("npc_dota_building");
 	dota.removeAll("npc_dota_neutral_spawner");
 	
-	for (var i in removeTargetName)
-	{
+	for (var i = 0; i < removeTargetName.length; i++){
 		var building = game.findEntityByTargetname(removeTargetName[i]);
 		if (building) dota.remove(building);
 	}
 	
-	for(var i = 0; i < 4; i++)
-	{
-		createUnit("dota_goodguys_fillers", dota.TEAM_RADIANT, -6910 + i * 200, -3050, 256, true);
+	for(var i = 0; i < 4; i++){
+		createUnit("dota_goodguys_fillers", dota.TEAM_RADIANT, -6920 + i * 200, -3050, 256, true);
 		createUnit("dota_goodguys_fillers", dota.TEAM_RADIANT, -3600, -6400 + i * 200, 256, true);
 		
-		createUnit("dota_goodguys_fillers", dota.TEAM_DIRE, 3200, 6050 - i * 200, 256, true);
-		createUnit("dota_goodguys_fillers", dota.TEAM_DIRE, 6000 + i * 200, 2650, 256, true);
+		createUnit("dota_badguys_fillers", dota.TEAM_DIRE, 3200, 6050 - i * 200, 256, true);
+		createUnit("dota_badguys_fillers", dota.TEAM_DIRE, 6000 + i * 200, 2650, 256, true);
 	}
 }
 
+function onPlayerKill(event){
+	for (var i = 1; i < 6; i++){
+		var client = server.userIdToClient(event.getInt("killer" + i + "_userid"));
+		if (!client) continue;
+		var playerID = client.netprops.m_iPlayerID;
+		if (!playerID) continue;
+		
+		playerManager.netprops.m_iUnreliableGoldRadiant[playerID] += 100;
+		playerManager.netprops.m_iUnreliableGoldDire[playerID] += 100;
+	}
+}
+
+function onPlayerKilled(event){
+	var playerID = event.getInt("PlayerID");
+	
+	playerManager.netprops.m_iUnreliableGoldRadiant[playerID] = playersGold[playerID]["Radiant"];
+	playerManager.netprops.m_iUnreliableGoldDire[playerID] = playersGold[playerID]["Dire"];
+}
+
 var x, y, z;
-function pos(client, args)
-{
+function pos(client, args){
 	var hero = client.netprops.m_hAssignedHero;
 	var playerId = client.netprops.m_iPlayerID;
-	//client.printToChat("pos msg");
-	
+
 	/*
 	x = hero.netprops.m_vecOrigin.x;
 	y = hero.netprops.m_vecOrigin.y;
 	z = hero.netprops.m_vecOrigin.z;
 	client.printToChat(x + " " + y + " " + z);
 	*/
-	//if (!playerManager)
-	//	client.printToChat("playerManager err");
-		
-	//var gold = playerManager.netprops.m_iReliableGold[5];
-	//var r = getPlayerResource(playerId, "m_iReliableGold");
-	//client.printToChat("gold " + gold);
-	//client.printToChat("gold2 " + r);
-	
-	//var lbbt = playerManager.netprops.m_iLastBuybackTime[playerId];
-	//client.printToChat("lbbt " + lbbt);
 }
 
-function createUnit(className, team, x, y, z, exact)
-{
+function createUnit(className, team, x, y, z, exact){
 	var ent = dota.createUnit(className, team);
 	if (!ent) return null;
 	
@@ -176,24 +169,21 @@ var removeTargetName =
 	"dota_goodguys_tower3_bot"
 ]
 
-function printToAll(string)
-{
-	var clients = getConnectedPlayingClients();
-	for (var i in clients)
+function printToAll(string){
+	var clients = getClients();
+	for (var i = 0; i < clients.length; i++)
 		clients[i].printToChat(string);
 }
 
-function getConnectedPlayingClients()
-{
-	var client, playing = [];
+function getClients(){
+	playing = [];
 	
-	for (var i = 0; i < server.clients.length; ++i)
-	{
-		client = server.clients[i];
+	for (var i = 0; i < server.clients.length; ++i){
+		var client = server.clients[i];
 
 		if (!client)
 			continue;
-
+			
 		if (!client.isInGame())
 			continue;
 
